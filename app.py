174e551eb5c0034c77f147d19ab7ad84b62a37f7
Ross Halpin <rosshalpin95@outlook.com>
2022-04-23 23:39:08 +0100

    Added wordcloud

--- app.py
+++ app.py
@@ -2,30 +2,63 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
 import plotly.express as px
+import os
 import pandas as pd
+from wordcloud import WordCloud
+
 app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+path = os.path.abspath(os.getcwd())
+data_path=f"{path}/static/data/"
+
+
+df = pd.read_csv(data_path+"cleaned_tweets.csv")
+
+def get_pie_chart(df):
+    sent_counts = df.sentiment.value_counts(normalize=True) * 100
+    data = pd.DataFrame(zip(sent_counts.keys(),sent_counts.values), columns=["sentiment", "percentage"])
+    pie_fig = px.pie(data, values='percentage', names='sentiment', title='Normalised Sentiment Breakdown', hole=.5, width=500)
+    pie_fig.update_layout(
+        font_family="monospace"
+    )
+    return pie_fig
+
+
+def get_word_cloud(text):
+    fig = px.imshow(WordCloud(max_words=50,
+        background_color="white",
+        scale=3
+        ).generate(text),title="Input Data Word Cloud")
+    fig.update_layout(
+        showlegend=False,
+        font_family="monospace"
+    )
+    fig.update_xaxes(visible=False)
+    fig.update_yaxes(visible=False)
+    
+    return fig
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Twitter data dashboard'),
 
     html.Div(children='''
-        Dash: A web application framework for your data.
+        Data Visualisation (DV) Common Module Assessment (CMA)
     '''),
+
 
     dcc.Graph(
         id='example-graph',
-        figure=fig
+        figure=get_pie_chart(df),
+        style={'width': '10md', 'height': '30md',"display": "inline-block","margin-left": "10"}
+    ),
+
+    dcc.Graph(
+        id="wordCloud",
+        figure=get_word_cloud(" ".join(df.text.str.split(expand=True).stack())),
+        style={"display": "inline-block","margin-left": "10"}
     )
 ])
 
